e3b66ff7c55a76aba4af1c866b16f658d32ca3ea
Adam Collier <adamcollierart@gmail.com>
2017-09-21 11:34:19 +0100

    vertical line only shows when there are tags

--- entry-footer.php
+++ entry-footer.php
@@ -1,5 +1,5 @@
 <footer class="entry-footer">
-<p class="cat-links"><?php the_category( ', ' ); ?></span>
-<span class="meta-sep"> | </span>
-<span class="tag-links"><?php the_tags(); ?></p>
+<p class="cat-links"><?php the_category( ', ' ); ?>
+<?php the_tags('<span class="meta-sep"> | </span> Tags: ', '', '')?>
+</p>
 </footer>
