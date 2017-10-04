117511802451e1cdf84db4af09c3f33655eb60b5
ishiiprints <adam.jacques@missguided.com>
2017-10-04 17:38:16 +0100

    :zap: tags seperated by comma

--- entry-footer.php
+++ entry-footer.php
@@ -1,5 +1,5 @@
 <footer class="entry-footer">
 <p class="cat-links"><?php the_category( ', ' ); ?>
-<?php the_tags('<span class="meta-sep"> | </span> Tags: ', '', '')?>
+<?php the_tags('<span class="meta-sep"> | </span> Tags: ', ', ', '')?>
 </p>
 </footer>
