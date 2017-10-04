e5a2303e1d6069d3392a3dfd686eeb30e6b57925
ishiiprints <adam.jacques@missguided.com>
2017-10-04 16:22:50 +0100

    :arrow_right: iss28; Pagination bug fixed.

--- nav-below-single.php
+++ nav-below-single.php
@@ -1,4 +1,30 @@
-<nav id="nav-below" class="navigation" role="navigation">
-<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ); ?></div>
-<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ); ?></div>
-</nav>
+<?php $previous = get_previous_post(); $next = get_next_post();
+if ( empty( $next ) ) {?>
+<div class="wrapperdiv">
+    <div class="float--left">
+        <a title="<?php echo esc_attr($previous->post_title); ?>" href="<?php echo get_permalink($previous->ID); ?>">
+            <h5>< previous post</h5>
+            <?php echo get_the_post_thumbnail($previous->ID, 'thumbnail'); ?>
+            <h4><?php echo esc_attr($previous->post_title); ?></h4>
+        </a>
+    </div>
+</div>
+<?php }else{ ?>
+
+<div class="wrapperdiv">
+    <div class="float--left">
+        <a title="<?php echo esc_attr($previous->post_title); ?>" href="<?php echo get_permalink($previous->ID); ?>">
+            <h5>< previous post</h5>
+            <?php echo get_the_post_thumbnail($previous->ID, 'thumbnail'); ?>
+            <h4><?php echo esc_attr($previous->post_title); ?></h4>
+        </a>
+    </div>
+    <div class="float--right">
+        <a title="<?php echo esc_attr($next->post_title); ?>" href="<?php echo get_permalink($next->ID); ?>">
+            <h5>next post ></h5>
+            <?php echo get_the_post_thumbnail($next->ID, 'thumbnail'); ?>
+            <h4><?php echo esc_attr($next->post_title); ?></h4>
+        </a>
+    </div>
+</div>
+<?php } ?>
--- single.php
+++ single.php
@@ -1,11 +1,8 @@
 <?php get_header(); ?>
-<!-- < ?php dynamic_sidebar( 'social-bar' ); ?> -->
 <section id="content" role="main">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<!-- not working :( -->
-		<div class="post-nav"><p><?php posts_nav_link('&#8734;','Go Forward In Time','Go Back in Time'); ?></p></div>
 		<?php get_template_part( 'entry' ); ?>
-		<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
 	<?php endwhile; endif; ?>
 	<?php get_footer(); ?>
+	<?php get_template_part( 'nav', 'below-single' ); ?>
 </section>
