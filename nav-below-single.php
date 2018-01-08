c9196ea7396eba7e2672012db932449381be362c
ishiiprints <adam.jacques@missguided.com>
2018-01-08 17:20:57 +0000

    :raised_hands: fixed lil product bug

--- nav-below-single.php
+++ nav-below-single.php
@@ -16,14 +16,14 @@ if ( empty( $next ) ) {?>
         <a title="<?php echo esc_attr($previous->post_title); ?>" href="<?php echo get_permalink($previous->ID); ?>">
             <h5>< previous post</h5>
             <?php echo get_the_post_thumbnail($previous->ID, 'thumbnail'); ?>
-            <h4><?php echo esc_attr($previous->post_title); ?></h4>
+            <h5><?php echo esc_attr($previous->post_title); ?></h5>
         </a>
     </div>
     <div class="float--right">
         <a title="<?php echo esc_attr($next->post_title); ?>" href="<?php echo get_permalink($next->ID); ?>">
             <h5>next post ></h5>
             <?php echo get_the_post_thumbnail($next->ID, 'thumbnail'); ?>
-            <h4><?php echo esc_attr($next->post_title); ?></h4>
+            <h5><?php echo esc_attr($next->post_title); ?></h5>
         </a>
     </div>
 </div>
