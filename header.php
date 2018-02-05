b9b409708f8af9491e635ac81faa47541cd941a3
ishiiprints <adam.jacques@missguided.com>
2018-02-05 14:52:46 +0000

    GTM added

--- header.php
+++ header.php
@@ -4,10 +4,21 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="theme-color" content="#ffffff"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TN84JK3');</script>
+<!-- End Google Tag Manager -->
 <?php wp_head(); ?>
 <title><?php wp_title(''); ?></title>
 </head>
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TN84JK3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <!-- facebook SDK -->
 	<script>
 	  window.fbAsyncInit = function() {
